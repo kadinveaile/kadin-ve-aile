8a3856df71b8fb745915c6e6a5425eefbc01fd48
kadinveaile <gulzafer389@gmail.com>
2026-01-22 22:24:07 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("Sihir Başladı: Kadın ve Aile Topluluğu Sitesi Yüklendi.");
-    
-    // İleride buraya 'Kâhin' formunun gönderim animasyonu eklenebilir.
-});
